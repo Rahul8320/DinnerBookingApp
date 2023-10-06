12d75437a8d907d1fe156e1428490ee23eedaec9
Rahul Dey <rahuldey832000@gmail.com>
2023-10-06 20:10:02 +0530

    Add Global Error Handeling Middelware

--- DinnerBooking.Api/Program.cs
+++ DinnerBooking.Api/Program.cs
@@ -1,4 +1,5 @@
 using DinnerBooking.Api.AutoMapper;
+using DinnerBooking.Api.Middleware;
 using DinnerBooking.Application;
 using DinnerBooking.Infrastructure;
 
@@ -13,20 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 }
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-{
+    app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
