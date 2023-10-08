fa7ef52af5603507e8c3a43dc1dc00e3cac231f8
Rahul Dey <rahuldey832000@gmail.com>
2023-10-08 12:48:59 +0530

    Add Jwt Barer Authentication

--- DinnerBooking.Infrastructure/DependencyInjection.cs
+++ DinnerBooking.Infrastructure/DependencyInjection.cs
@@ -6,23 +6,49 @@ using DinnerBooking.Infrastructure.Persistence;
 using DinnerBooking.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
 
-namespace DinnerBooking.Infrastructure
+namespace DinnerBooking.Infrastructure;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
     {
-        public static IServiceCollection AddInfrastructure(
-            this IServiceCollection services,
-            ConfigurationManager configuration)
-        {
-            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddAuth(configuration);
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
-            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
-            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddScoped<IUserRepository, UserRepository>();
 
-            services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
 
-            return services;
-        }
+    public static IServiceCollection AddAuth(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSettings.Secret)
+                )
+            });
+
+        return services;
     }
 }
